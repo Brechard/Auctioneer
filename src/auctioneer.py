b0a9d3796049fb72340dd3fb12cddc6d3c2ee4d2
Giorgos Patsiaouras <giorgospatsiaouras@gmail.com>
2018-12-07 17:28:27 +0100

    Some comments

--- src/auctioneer.py
+++ src/auctioneer.py
@@ -14,8 +14,11 @@ class Auctioneer:
         self.max_starting_price = 100
         self.penalty_factor = 0.1
 
+        # If level commitment is activated sellers cannot cancel a won auction
         self.level_flag = level_comm_flag
-        self.buyers_flag = [False for buyer in range(self.n_buyers)]
+        # Buyers ability to participate in an auction
+        self.buyers_cannot_participate = [False for buyer in range(self.n_buyers)]
+        # The type of item that each of the sellers is going to sell
         self.sellers_types = [random.sample(self.m_item_types, 1)[0] for seller in range(self.k_sellers)]
 
         self.bidding_factor = np.random.uniform(1, 2, size=(self.n_buyers, len(self.m_item_types), self.k_sellers))
@@ -34,11 +37,14 @@ class Auctioneer:
     def start_auction(self):
         # TODO fill market price, buyers and sellers profit matrix
         for auction_round in range(self.r_rounds):
-            self.buyers_flag = [False for buyer in range(self.n_buyers)]
+
+            # Reset all buyers ability to participate in an auction
+            self.buyers_cannot_participate = [False for buyer in range(self.n_buyers)]
+
             for seller in range(self.k_sellers):
                 buyers_bid, item, n_buyer_auction, starting_price, total_bid = self.calculate_auction_parameters(seller)
                 for buyer in range(self.n_buyers):
-                    if self.buyers_flag[buyer] and not self.level_flag:
+                    if self.buyers_cannot_participate[buyer] and not self.level_flag:
                         continue
                     n_buyer_auction += 1
                     bid = self.calculate_bid(buyer, item, seller, starting_price)
@@ -49,7 +55,7 @@ class Auctioneer:
                 winner, price_to_pay = self.choose_winner(buyers_bid, market_price)
 
                 if not self.level_flag:
-                    self.buyers_flag[winner] = True
+                    self.buyers_cannot_participate[winner] = True
 
                 self.update_alphas(winner, seller, item)
                 self.market_price[auction_round, seller] = market_price
@@ -97,44 +103,6 @@ class Auctioneer:
             else:
                 self.bidding_factor[buyer, item, seller] *= self.increase_bidding_factor[buyer]
 
-    """
-    Initialize:
-    Number of Items, Sellers, Buyers
-    """
-
-    def initialize_variables(self):
-        # TODO Implement initialize_variables
-        pass
-
-    """
-    Initialize alpha values for each buyer and seller and item
-    """
-
-    def initialize_alpha_values(self):
-        # TODO Implement initialize_alpha_values
-        pass
-
-    """
-    Initialize delta values
-    """
-
-    def initialize_delta_values(self):
-        # TODO Implement initialize_delta_values
-        pass
-
-    """
-    Perform simulation
-    """
-
-    def run_simulation(self):
-        # Perform initializations
-        self.initialize_variables()
-        self.initialize_alpha_values()
-        self.initialize_delta_values()
-
-        # TODO Implement rest of the simulation
-        pass
-
     def print_outcome(self):
         # TODO Implement print_outcome
         print("The market price history is:")
@@ -148,5 +116,4 @@ class Auctioneer:
 
 if __name__ == '__main__':
     auctioneer = Auctioneer()
-    # auctioneer.run_simulation()
     auctioneer.print_outcome()
